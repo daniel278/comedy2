c3c0789da06a0ef208029cd9b2215c0573f16866
daniel <danielfernandocorrea1@gmail.com>
2019-12-10 16:48:28 -0500

    termine el home, falta optimizarlo solamente

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ import comediante_recomendado from './assets/img/comediantes/camilo_sanchez.jpg'
 
 
 function App() {
+  const comediante_recomendado_texto = "Camilo Sanchez"
+  const comediante_recomendado_instagram = "@dejemequieto"
+  const comediante_recomendado_instagram_link = "https://www.instagram.com/dejemequieto/?hl=es-la";
   return (
     <div className="App">
       <header>
@@ -36,14 +39,44 @@ function App() {
           </div>
           <div className="recomendacion_body card-body">
             <div className="perfil_comediante_recomendado">
-              <div>Camilo Sanchez</div>
-              <div><a href="https://www.instagram.com/dejemequieto/?hl=es-la">@dejemequieto</a></div>
+              <div>{comediante_recomendado_texto}</div>
+              <div><a href={comediante_recomendado_instagram_link}>{comediante_recomendado_instagram}</a></div>
               <div><img className="foto_comediante_recomendado" src = { comediante_recomendado }></img></div>
             </div>
-            <iframe id='ytplayer' src="https://www.youtube.com/embed/_AAqUamNQA0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe id='ytplayer' src="https://www.youtube.com/embed/_AAqUamNQA0" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
           </div>
-          <div class="card-footer">
+          <div className="card-footer">
             <a href="https://www.youtube.com/watch?v=_AAqUamNQA0" className="btn btn-danger">Ver en youtube</a>
+          </div>
+        </div>
+        <div className="curador_picks">
+          <div className="card-header">
+            ¿Que hay de nuevo?
+          </div>
+          <div className="fila_1">
+            <div className="card">
+              <div className="recomendacion_body card-body">
+                <iframe id="ytplayer" src="https://www.youtube.com/embed/1sLsxZSRMsI" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>              
+              </div>
+              <div className="card-footer">
+                <a href="https://www.youtube.com/watch?v=1sLsxZSRMsI" className="btn btn-danger">Ver en youtube</a>
+              </div>
+            </div>
+            <div className="card">
+              <div className="recomendacion_body card-body">
+              <iframe id="ytplayer" src="https://www.youtube.com/embed/RW03ahBWhuY" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>              </div>
+              <div className="card-footer">
+                <a href="https://www.youtube.com/watch?v=RW03ahBWhuY" className="btn btn-danger">Ver en youtube</a>
+              </div>
+            </div>
+            <div className="card">
+              <div className="recomendacion_body card-body">
+              <iframe id="ytplayer" src="https://www.youtube.com/embed/ZGgDpx4Wg-U" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>              
+              </div>
+              <div className="card-footer">
+                <a href="https://www.youtube.com/watch?v=ZGgDpx4Wg-U" className="btn btn-danger">Ver en youtube</a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
